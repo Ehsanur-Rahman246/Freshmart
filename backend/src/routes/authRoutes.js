b9ecebe9d7abfee46d4b3e950e8bc4aeb8e20939
Ehsanur-Rahman246 <rahmanehsanur246@gmail.com>
2026-09-02 23:09:15 +0600

    added auth controllers, middleware and routes, the registration, login-logout, verification, reset pass backend complete

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,27 @@
-import express from 'express';
-import { deleteUserByEmail, login, logout, register } from '../controllers/authControllers.js';
+import express from "express";
+import {
+  checkAuth,
+  deleteUserByEmail,
+  login,
+  logout,
+  register,
+  resetPassword,
+  sendResetPasswordOtp,
+  sendVerificationOtp,
+  verifyUser,
+} from "../controllers/authControllers.js";
+import userAuth from "../middleware/userAuth.js";
 
 const authRouter = express.Router();
 
-authRouter.post('/register', register);
-authRouter.post('/login', login);
-authRouter.post('/logout', logout);
+authRouter.post("/register", register);
+authRouter.post("/login", login);
+authRouter.post("/logout", logout);
 authRouter.delete("/deleteUser", deleteUserByEmail);
+authRouter.post("/send-verification-otp", userAuth, sendVerificationOtp);
+authRouter.post("/verify-account", userAuth, verifyUser);
+authRouter.get("/check-auth", userAuth, checkAuth);
+authRouter.post("/send-reset-otp", sendResetPasswordOtp);
+authRouter.post("/reset-password", resetPassword);
 
 export default authRouter;
